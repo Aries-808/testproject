7a7ed13235ab089b248d1f29fca34c5cdad0f8fb
Victory Ajigbotosho <ajigbotoshovictory@gmail.com>
2024-03-22 09:01:01 +0100

    changes

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,17 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD:testproject/testproject/urls.py
     path('', include('research_repo_app.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-=======
-    path('', include('research_repo_app.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
->>>>>>> laspotech_project/panywhere:lasustechrepo/lasustechrepo/urls.py
